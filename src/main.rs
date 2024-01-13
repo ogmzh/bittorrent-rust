b7d6f2696fc6bcf8718c4647ccd731a31347fb97
ogmzh <omg.gio@gmail.com>
2024-01-13 17:33:46 +0100

    decode string

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,13 @@
-use serde_json;
 use std::env;
-
-// Available if you need it!
-// use serde_bencode
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+    if let Some((prefix, rest)) = encoded_value.split_once(':') {
+        if prefix.parse::<usize>().is_ok() {
+            return serde_json::Value::String(rest.to_string());
+        }
     }
+    panic!("Unhandled encoded value: {}", encoded_value)
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
@@ -29,9 +20,9 @@ fn main() {
         println!("Logs from your program will appear here!");
 
         // Uncomment this block to pass the first stage
-        // let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", decoded_value.to_string());
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+        println!("{}", decoded_value);
     } else {
         println!("unknown command: {}", args[1])
     }

fe999248647c9b3d1924046d7af10a37f9273dd5
ogmzh <omg.gio@gmail.com>
2024-01-13 19:04:36 +0100

    decode dicts

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, collections::HashMap};
 
 // instead of parsing these strings manually and recursively (for lists), we could just call
 // but I'm doing it manually for some rust practice.
@@ -21,14 +21,38 @@ fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
         Some('l') => {
             let mut values = Vec::new();
             let mut remainder = encoded_value.split_at(1).1; // lists look like l5:helloi52ee
-            while !remainder.is_empty() && !remainder.starts_with('e') {
-                // e character is a terminator
+            while !remainder.starts_with('e') {  // e character is the terminator
                 let (value, rest) = decode_bencoded_value(remainder);
                 values.push(value);
                 remainder = rest;
             }
             // return the list with whatever is left after in the encoded string, as the list has been terminated in the while with 'e'
-            return (values.into(), &remainder[1..]);
+            return (values.into(), &remainder[1..]); // skip the e terminating the list
+        }
+        Some('d') => {
+            let mut map = serde_json::Map::new();
+            let mut remainder = encoded_value.split_at(1).1; // dictionaries look like d3:foo3:bar5:helloi52ee
+            let mut count = 0;
+            let mut key: String = String::new();
+            let mut map_value: serde_json::Value;
+            while !remainder.starts_with('e') {
+                let (value, rest) = decode_bencoded_value(remainder);
+                if count == 0 {
+                    match value {
+                        serde_json::Value::String(k) => key = k,
+                        k => {
+                            panic!("Dict keys must be strings, not {k:?}");
+                        }
+                    };
+                    count += 1;
+                } else {
+                    map_value = value;
+                    map.insert(key.clone(), map_value);
+                    count = 0;
+                }
+                remainder = rest;
+            }
+            return (map.into(), &remainder[1..]); // skip the e terminating the dict
         }
         Some('0'..='9') => {
             if let Some((length, rest)) = encoded_value.split_once(':') {
